393515fe7b373d678cad8e5ecb0cc0f4ce1e59a9
Georgich88 <georgeisaev@gmail.com>
2020-10-07 14:30:17 +0700

    Understanding CopyOnWrite Collections

--- src/main/java/com/georgeisaev/ocp/concurrency/collections/copyonwrite/Shelf.java
+++ src/main/java/com/georgeisaev/ocp/concurrency/collections/copyonwrite/Shelf.java
@@ -1,24 +1,31 @@
 package com.georgeisaev.ocp.concurrency.collections.copyonwrite;
 
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.stream.Collectors;
 
 import static java.util.Objects.hash;
 
-interface ShelfObject {
-
-}
-
+/**
+ * Illustrate a concurrent access to data stored in {@code CopyOnWrite} collections.
+ *
+ * @param <T> type of the library index
+ * @author Georgy Isaev
+ * @version 1.0.0
+ */
 public class Shelf<T> {
 
-    private static final int MAX_SHELF_SIZE = 1 << 4; // 16 books
+    static final int MAX_SHELF_SIZE = 1 << 4; // 16 books
+
+    // Fields
+
     private final int capacity;
     private T index;
     private CopyOnWriteArrayList<Book> books;
     private CopyOnWriteArraySet<Author> authors;
+
+    // Constructors
 
     public Shelf(T index) {
         this(index, MAX_SHELF_SIZE);
@@ -31,19 +38,38 @@ public class Shelf<T> {
         this.authors = new CopyOnWriteArraySet<>();
     }
 
-    void putOn(String name, String... authorNames) {
-        Book bookToAdd = new Book(name, authorNames);
-        books.add(new Book(name, authorNames));
+    // Modification methods
+
+    void putOn(Book bookToAdd) {
+        books.add(bookToAdd);
         authors.addAll(bookToAdd.getAuthors());
     }
 
-    void takeFrom(String name, String... authorNames) {
+    List<Book> takeFrom(String name) {
+        List<Book> removedBooks =
+                books.stream()
+                        .filter(book -> name.equals(book.toString()))
+                        .collect(Collectors.toList());
+        Set<Author> authorsToRemove =
+                removedBooks.stream()
+                        .map(Book::getAuthors)
+                        .flatMap(Collection::stream)
+                        .collect(Collectors.toSet());
+        authors.removeAll(authorsToRemove);
+        return removedBooks;
+    }
 
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Shelf.class.getSimpleName() + "[", "]")
+                .add("index=" + index)
+                .add("books=" + books)
+                .toString();
     }
 
 }
 
-class Book implements ShelfObject {
+class Book {
 
     private final String name;
     private final Set<Author> authors;
@@ -58,6 +84,10 @@ class Book implements ShelfObject {
     protected Book(String name, Author... authors) {
         this.name = name;
         this.authors = Set.of(authors);
+    }
+
+    public String getName() {
+        return name;
     }
 
     public Set<Author> getAuthors() {
@@ -76,6 +106,11 @@ class Book implements ShelfObject {
     @Override
     public int hashCode() {
         return hash(name, authors);
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 
 }
